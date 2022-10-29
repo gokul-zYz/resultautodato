3feb44ad03163a6e3786db2353d337ad7d0ce416
GOKUL SUBRAMANIAN <96585296+GOKUL69@users.noreply.github.com>
2022-10-30 01:32:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from selenium import webdriver
+from discord_webhook import DiscordWebhook,DiscordEmbed
+from selenium.webdriver.common.by import By
+import time
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+var=401
+def cord(result,colr):
+  webhookurl="https://discord.com/api/webhooks/1035595915894984855/qhPVZeypFgsNwvNvAn3mwExKrvyRCv6r2ODMl2HM4ZD7JpBG7Lsccr7mc9DaCP7hbkCY"
+  webhook=DiscordWebhook(url=webhookurl)
+  embed=DiscordEmbed(title="RESULT",description=result,color=colr)
+  webhook.add_embed(embed)
+  webhook.execute()
+def result(num):
+  if num<495:
+   f="19tH0"+str(num)
+   driver.get("http://exam.pondiuni.edu.in/results/")
+   driver.find_element(By.XPATH, '//*[@id="reg_no"]').send_keys("19tH0"+str(num))
+   driver.find_element(By.XPATH, '//*[@id="exam"]').send_keys("seventh")
+   driver.find_element(By.XPATH, '//*[@id="print_app_form"]/span').click()
+   time.sleep(1)
+   try:
+    driver.switch_to.alert.accept()
+    cord("NOT YET CAME FOR :"+f,"ED4245")
+   except:
+    print("SUCCESFULLY RETRIEVED")
+    cord("CAME FOR :"+f,"57F287")
+
+   result(num+1) 
+  else:
+    result(var)
+    
+result(var)
+driver.close()
+
+
+
+
+
+
+
